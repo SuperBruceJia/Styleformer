de1e36177ff91f30119b0e2303f4100d8e28ab8b
SuperBruceJia <shuyuej@ieee.org>
2023-12-14 11:18:02 -0500

    update

--- styleformer.py
+++ styleformer.py
@@ -7,64 +7,64 @@ from transformers import (
     AutoModelForSequenceClassification,
 )
 
-model_tag = 'prithivida/parrot_adequacy_model'
-ftc_model_tag = "prithivida/formal_to_informal_styletransfer"
-ctf_model_tag = "prithivida/informal_to_formal_styletransfer"
-atp_model_tag = "prithivida/active_to_passive_styletransfer"
-pta_model_tag = "prithivida/passive_to_active_styletransfer"
+adequacy_tag = 'prithivida/parrot_adequacy_model'
+ftc_tag = "prithivida/formal_to_informal_styletransfer"
+ctf_tag = "prithivida/informal_to_formal_styletransfer"
+atp_tag = "prithivida/active_to_passive_styletransfer"
+pta_tag = "prithivida/passive_to_active_styletransfer"
 
 
 class Adequacy:
     def __init__(self):
-        self.tokenizer = AutoTokenizer.from_pretrained(model_tag)
-        self.adequacy_model = AutoModelForSequenceClassification.from_pretrained(model_tag, torch_dtype=torch.float32)
+        self.tokenizer = AutoTokenizer.from_pretrained(adequacy_tag)
+        self.model = AutoModelForSequenceClassification.from_pretrained(adequacy_tag, torch_dtype=torch.float32)
 
-    def filter(self, input_phrase, para_phrases, adequacy_threshold, device="cpu"):
-        top_adequacy_phrases = []
-        for para_phrase in para_phrases:
-            x = self.tokenizer(input_phrase, para_phrase, return_tensors='pt', max_length=128, truncation=True)
-            self.adequacy_model = self.adequacy_model.to(device)
+    def filter(self, input_phrase, para_phrases, threshold, device="cpu"):
+        top_phrases = []
+        for phrase in para_phrases:
+            x = self.tokenizer(input_phrase, phrase, return_tensors='pt', max_length=128, truncation=True)
+            self.model = self.model.to(device)
 
-            logits = self.adequacy_model(**x).logits
+            logits = self.model(**x).logits
             probs = logits.softmax(dim=1)
-            prob_label_is_true = probs[:, 1]
+            prob_label = probs[:, 1]
 
-            adequacy_score = prob_label_is_true.item()
-            if adequacy_score >= adequacy_threshold:
-                top_adequacy_phrases.append(para_phrase)
+            score = prob_label.item()
+            if score >= threshold:
+                top_phrases.append(phrase)
 
-        return top_adequacy_phrases
+        return top_phrases
 
-    def score(self, input_phrase, para_phrases, adequacy_threshold, device="cpu"):
-        adequacy_scores = {}
-        for para_phrase in para_phrases:
-            x = self.tokenizer(input_phrase, para_phrase, return_tensors='pt', max_length=128, truncation=True)
+    def score(self, input_phrase, para_phrases, threshold, device="cpu"):
+        scores = {}
+        for phrase in para_phrases:
+            x = self.tokenizer(input_phrase, phrase, return_tensors='pt', max_length=128, truncation=True)
             x = x.to(device)
-            self.adequacy_model = self.adequacy_model.to(device)
+            self.model = self.model.to(device)
 
-            logits = self.adequacy_model(**x).logits
+            logits = self.model(**x).logits
             probs = logits.softmax(dim=1)
-            prob_label_is_true = probs[:, 1]
+            prob_label = probs[:, 1]
 
-            adequacy_score = prob_label_is_true.item()
-            if adequacy_score >= adequacy_threshold:
-                adequacy_scores[para_phrase] = adequacy_score
+            score = prob_label.item()
+            if score >= threshold:
+                scores[phrase] = score
 
-        return adequacy_scores
+        return scores
 
 
 class StyleFormer:
     def __init__(self, style=0):
         self.style = style
 
-    def transfer(self, sen, quality_filter=0.95, max_candidates=5):
+    def transfer(self, sen, filter=0.95, candidates=5):
         device = "cpu"
         if self.style == 0:
-            output = self._casual_to_formal(sen, device, quality_filter, max_candidates)
+            output = self._casual_to_formal(sen, device, filter, candidates)
             return output
 
         elif self.style == 1:
-            output = self._formal_to_casual(sen, device, quality_filter, max_candidates)
+            output = self._formal_to_casual(sen, device, filter, candidates)
             return output
 
         elif self.style == 2:
@@ -75,9 +75,9 @@ class StyleFormer:
             output = self._passive_to_active(sen, device)
             return output
 
-    def _formal_to_casual(self, sen, device, quality_filter, max_candidates):
-        self.ftc_tokenizer = AutoTokenizer.from_pretrained(ftc_model_tag)
-        self.ftc_model = AutoModelForSeq2SeqLM.from_pretrained(ftc_model_tag, torch_dtype=torch.float32)
+    def _formal_to_casual(self, sen, device, filter, candidates):
+        self.ftc_tokenizer = AutoTokenizer.from_pretrained(ftc_tag)
+        self.ftc_model = AutoModelForSeq2SeqLM.from_pretrained(ftc_tag, torch_dtype=torch.float32)
 
         ftc_prefix = "transfer Formal to Casual: "
         src_sen = sen
@@ -94,26 +94,24 @@ class StyleFormer:
             top_k=50,
             top_p=0.95,
             early_stopping=True,
-            num_return_sequences=max_candidates
+            num_return_sequences=candidates
         )
 
         gen_sen = set()
         for pred in preds:
             gen_sen.add(self.ftc_tokenizer.decode(pred, skip_special_tokens=True).strip())
 
-        print('gen_sen: ', gen_sen)
-
         self.adequacy = Adequacy()
-        scored_phrases = self.adequacy.score(src_sen, list(gen_sen), quality_filter, device)
-        ranked_sen = sorted(scored_phrases.items(), key=lambda x: x[1], reverse=True)
-        if len(ranked_sen) > 0:
-            return ranked_sen[0][0]
+        phrases = self.adequacy.score(src_sen, list(gen_sen), filter, device)
+        ranking = sorted(phrases.items(), key=lambda x: x[1], reverse=True)
+        if len(ranking) > 0:
+            return ranking[0][0]
         else:
             return None
 
-    def _casual_to_formal(self, sen, device, quality_filter, max_candidates):
-        self.ctf_tokenizer = AutoTokenizer.from_pretrained(ctf_model_tag)
-        self.ctf_model = AutoModelForSeq2SeqLM.from_pretrained(ctf_model_tag, torch_dtype=torch.float32)
+    def _casual_to_formal(self, sen, device, filter, candidates):
+        self.ctf_tokenizer = AutoTokenizer.from_pretrained(ctf_tag)
+        self.ctf_model = AutoModelForSeq2SeqLM.from_pretrained(ctf_tag, torch_dtype=torch.float32)
 
         ctf_prefix = "transfer Casual to Formal: "
         src_sen = sen
@@ -130,7 +128,7 @@ class StyleFormer:
             top_k=50,
             top_p=0.95,
             early_stopping=True,
-            num_return_sequences=max_candidates
+            num_return_sequences=candidates
         )
 
         gen_sen = set()
@@ -138,16 +136,16 @@ class StyleFormer:
             gen_sen.add(self.ctf_tokenizer.decode(pred, skip_special_tokens=True).strip())
 
         self.adequacy = Adequacy()
-        scored_phrases = self.adequacy.score(src_sen, list(gen_sen), quality_filter, device)
-        ranked_sen = sorted(scored_phrases.items(), key=lambda x: x[1], reverse=True)
-        if len(ranked_sen) > 0:
-            return ranked_sen[0][0]
+        phrases = self.adequacy.score(src_sen, list(gen_sen), filter, device)
+        ranking = sorted(phrases.items(), key=lambda x: x[1], reverse=True)
+        if len(ranking) > 0:
+            return ranking[0][0]
         else:
             return None
 
     def _active_to_passive(self, sen, device):
-        self.atp_tokenizer = AutoTokenizer.from_pretrained(atp_model_tag)
-        self.atp_model = AutoModelForSeq2SeqLM.from_pretrained(atp_model_tag, torch_dtype=torch.float32)
+        self.atp_tokenizer = AutoTokenizer.from_pretrained(atp_tag)
+        self.atp_model = AutoModelForSeq2SeqLM.from_pretrained(atp_tag, torch_dtype=torch.float32)
 
         atp_prefix = "transfer Active to Passive: "
         sen = atp_prefix + sen
@@ -169,8 +167,8 @@ class StyleFormer:
         return self.atp_tokenizer.decode(preds[0], skip_special_tokens=True).strip()
 
     def _passive_to_active(self, sen, device):
-        self.pta_tokenizer = AutoTokenizer.from_pretrained(pta_model_tag)
-        self.pta_model = AutoModelForSeq2SeqLM.from_pretrained(pta_model_tag, torch_dtype=torch.float32)
+        self.pta_tokenizer = AutoTokenizer.from_pretrained(pta_tag)
+        self.pta_model = AutoModelForSeq2SeqLM.from_pretrained(pta_tag, torch_dtype=torch.float32)
 
         pta_prefix = "transfer Passive to Active: "
         sen = pta_prefix + sen
